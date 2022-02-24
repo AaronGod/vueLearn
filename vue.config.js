183c5fe2671d9eba71ce566f5a4a7174a276c3c6
aaronpeng <aaronpeng.gan@foxmail.com>
2022-02-24 12:57:22 +0800

    关闭eslint检查

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const {
 } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
+    lintOnSave: false, // 关闭eslint检查
     devServer: {
         port: 3000
     }
